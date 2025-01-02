9bbffca43d2906bab09558999cfc9802dbe22f67
Surabhi <surabhimhamane05@gmail.com>
2025-01-02 22:22:53 +0530

    rounding off earnings

--- src/pages/dashbord/Dashboard.jsx
+++ src/pages/dashbord/Dashboard.jsx
@@ -3,8 +3,7 @@ import axios from "axios";
 import { server } from "../../index.js"; // Assuming your server URL
 import styles from "./Dashboard.module.css"; // Modular CSS file
 import { TbMoneybag } from "react-icons/tb";
-import { GiReceiveMoney , GiTakeMyMoney , GiOpenTreasureChest} from "react-icons/gi";
-
+import { GiReceiveMoney, GiTakeMyMoney, GiOpenTreasureChest } from "react-icons/gi";
 
 const Dashboard = () => {
   const [user, setUser] = useState(null);
@@ -32,47 +31,46 @@ const Dashboard = () => {
 
   return (
     <div className={styles.dashboard}>
-    
       <h1 className={styles.userName}>{user.name}</h1>
 
       <div className={styles.section}>
         <div className={styles.earningBox}>
           <div className={styles.earnLeft}>
-            <p>{user.earnings.today || 0}/-</p>
+            <p>{Math.round(user.earnings.today || 0)}/-</p>
             <h4>Today's Earnings</h4>
           </div>
           <div className={styles.earnRight}>
-          <TbMoneybag />
+            <TbMoneybag />
           </div>
         </div>
 
         <div className={styles.earningBox}>
           <div className={styles.earnLeft}>
-            <p>{user.earnings.month || 0}/-</p>
-            <h4>30 days Earnings</h4>
+            <p>{Math.round(user.earnings.month || 0)}/-</p>
+            <h4>30 Days Earnings</h4>
           </div>
           <div className={styles.earnRight}>
-            <GiReceiveMoney/>
+            <GiReceiveMoney />
           </div>
         </div>
 
         <div className={styles.earningBox}>
           <div className={styles.earnLeft}>
-            <p>{user.earnings.week || 0}/-</p>
-            <h4>7 days Earnings</h4>
+            <p>{Math.round(user.earnings.week || 0)}/-</p>
+            <h4>7 Days Earnings</h4>
           </div>
           <div className={styles.earnRight}>
-            <GiTakeMyMoney/>
+            <GiTakeMyMoney />
           </div>
         </div>
 
         <div className={styles.earningBox}>
           <div className={styles.earnLeft}>
-            <p>{user.earnings.total || 0}/-</p>
+            <p>{Math.round(user.earnings.total || 0)}/-</p>
             <h4>Total Earnings</h4>
           </div>
           <div className={styles.earnRight}>
-            <GiOpenTreasureChest/>
+            <GiOpenTreasureChest />
           </div>
         </div>
       </div>
