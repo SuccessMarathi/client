e6a76f06ef7c98d7c84b64cf3ee6bc7c876c1ab2
Speedysays01 <surabhimhamane05@gmail.com>
2025-02-03 20:35:07 +0530

    Added images to the leaderboard

--- src/pages/leaderboard/Leaderboard.jsx
+++ src/pages/leaderboard/Leaderboard.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./Leaderboard.module.css";
-import { server } from "../../index.js"; // Assuming server URL
+import { server } from "../../index.js"; 
 import { FaCrown } from "react-icons/fa";
-
 
 const Leaderboard = () => {
   const [leaderboard, setLeaderboard] = useState([]);
@@ -34,16 +33,15 @@ const Leaderboard = () => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-      <FaCrown/>
-      <h1>
-        LEADERBOARD
-        </h1>
-       <FaCrown/>
+        <FaCrown />
+        <h1 className={styles.title}>LEADERBOARD</h1>
+        <FaCrown />
       </div>
       <table className={styles.table}>
         <thead>
           <tr>
             <th>Rank</th>
+            <th>Profile</th>
             <th>Name</th>
             <th>Total Earnings</th>
           </tr>
@@ -52,8 +50,19 @@ const Leaderboard = () => {
           {leaderboard.map((user, index) => (
             <tr key={user._id}>
               <td>{index + 1}</td>
+              <td>
+                {user.profileImage ? (
+                  <img
+                    src={user.profileImage}
+                    alt={`${user.name}'s Profile`}
+                    className={styles.profileImage}
+                  />
+                ) : (
+                  <div className={styles.defaultAvatar}>👤</div>
+                )}
+              </td>
               <td>{user.name}</td>
-              <td>{Math.round(user.earnings.total)}Rs</td>
+              <td>{Math.round(user.totalEarnings)} Rs</td>
             </tr>
           ))}
         </tbody>
